d25a41c77fe54a8e9fe6b8a50e0d359356e80e86
Mira Javora <mirajavora@gmail.com>
2013-01-28 21:09:52 +0000

    Initial work on the image formatter

---
+++ WebApiBlog/Core/MediaFormatters/ImageFormatter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+
+namespace WebApiBlog.Core.MediaFormatters
+{
+    public class ImageFormatter : MediaTypeFormatter
+    {
+        public ImageFormatter()
+        {
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue("image/jpeg"));   
+        }
+
+        public override bool CanReadType(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanWriteType(Type type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
